b7ced8a4987eaed500cfe040bc2fd547446edb3f
Kehinde Fasunle <kfasunle@gmail.com>
2024-06-09 06:11:10 -0400

    test/logger-handler: add logger for testing purposes

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -4,17 +4,24 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
 )
 
 type RequestPayload struct {
-	Action string      `json:action`
-	Auth   AuthPayload `json:auth,omitempty`
+	Action string      `json:"action"`
+	Auth   AuthPayload `json:"auth,omitempty"`
+	Log    LogPayload  `json:"log,omitempty"`
 }
 
 type AuthPayload struct {
-	Email    string `json:email`
-	Password string `json:password`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type LogPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
 }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
@@ -43,12 +50,56 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 	switch requestPayload.Action {
 	case "auth":
 		app.authenticate(w, requestPayload.Auth)
+	case "log":
+		app.logEvent(w, requestPayload.Log)
 
 	default:
 		app.errorJSON(w, errors.New("unknown action"))
 	}
 }
 
+func (app *Config) logEvent(w http.ResponseWriter, l LogPayload) {
+	// create json to be sent to log microservice
+	jsonData, _ := json.MarshalIndent(l, "", "\t")
+	logServiceURL := "http://logger-service/log"
+
+	// create requestto be sent to the log microservice
+	request, err := http.NewRequest("POST", logServiceURL, bytes.NewBuffer(jsonData))
+
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	client := http.Client{}
+	response, err := client.Do(request)
+
+	fmt.Println(response.Status)
+
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	defer response.Body.Close()
+
+	// make sure that the correct status code is being returned
+
+	if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, errors.New("log service returned incorrect status code"))
+		return
+	}
+
+	// build success response
+
+	log := jsonResponse{
+		Error:   false,
+		Message: "Logged ✅",
+		Data:    l,
+	}
+
+	app.writeJSON(w, http.StatusAccepted, log)
+}
 func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 	// create json to be sent to auth microservice
 	jsonData, _ := json.MarshalIndent(a, "", "\t")
